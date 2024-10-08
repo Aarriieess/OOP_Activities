067d9e184b28b7c1d672f22d02581633182ab6e4
Aries <134371772+Aarriieess@users.noreply.github.com>
2024-10-08 10:03:19 +0800

    Add LA14

---
+++ LA14.py
@@ -0,0 +1,36 @@
+class Spiderman():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def describeSpiderman(self):
+        print(f"Name: {self.name} - Age: {self.age}")
+
+
+class Tobey(Spiderman):
+    def __init__(self, name, age, movieTitle):
+        super(). __init__(name, age)
+        self.movieTitle = movieTitle
+
+class Andrew(Spiderman):
+    def __init__(self, name, age, movieTitle):
+        super(). __init__(name, age)
+        self.movieTitle = movieTitle
+
+class Tom(Spiderman):
+    def __init__(self, name, age, movieTitle):
+        super(). __init__(name, age)
+        self.movieTitle = movieTitle
+
+
+tobey = Tobey("Tobey", 49, "Spiderman")
+andrew = Andrew("Andrew", 41, "The Amazing Spiderman")
+tom = Tom("Tom", 28, "Captain America: Civil War (2016)")
+
+spiderman = [tobey, andrew, tom]
+for item in spiderman:
+    print(f"{item.name.upper()} - {item.movieTitle}")
+
+# print(f"{tobey.name.upper()} - {tobey.movieTitle}")
+# print(f"{andrew.name.upper()} - {andrew.movieTitle}")
+# print(f"{tom.name.upper()} - {tom.movieTitle}")
